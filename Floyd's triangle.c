dbb22e094cac2fdcd7b563fe0ac1f52c080d0454
visal-ks <62129038+visal-ks@users.noreply.github.com>
2020-03-13 19:27:12 +0530

    Add files via upload

---
+++ Floyd's triangle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main( ) {
+    int i, j, k = 1;
+    printf("floyds triangle is\n");
+    for( i = 1; k <= 20; ++i ) {
+        for( j = 1; j <= i; ++j )      printf( "%d ", k++ );
+        printf( "\n\n" );
+    }
+    return 0;
+}
